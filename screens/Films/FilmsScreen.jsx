379fdca9b46ebb437edb48bba14c19cc037bde74
Jaya McMoore <hurifajri@gmail.com>
2020-03-19 16:25:20 +0700

    Finish films screen

--- screens/Films/FilmsScreen.jsx
+++ screens/Films/FilmsScreen.jsx
@@ -1,11 +1,126 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { ActivityIndicator, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { SCHEMA } from '../../constants/Api';
 import styles from './styles';
 
-export default function FilmsScreen() {
+export default function FilmsScreen({ navigation, route }) {
+  const [detail, setDetail] = React.useState([]);
+  const [relatedData, setRelatedData] = React.useState([]);
+  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+  const { name, url } = route.params;
+
+  // Set title for each selected category
+  const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
+
+  navigation.setOptions({
+    headerTitle: nameCapitalized,
+  });
+
+  React.useEffect(() => {
+    // Get detail by name
+    (async function getDetailByName() {
+      try {
+        const response = await fetch(`${url}?${SCHEMA}`);
+        const responseJson = await response.json();
+        const data = [responseJson];
+        setDetail(data);
+        fetchRelatedData(data[0].characters);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoadingComplete(true);
+      }
+    })();
+  }, []);
+
+  // Fetch related data of selected detail
+  const fetchRelatedData = urls => {
+    // map every url to promise of fetch
+    let requests = urls.map(url =>
+      fetch(url)
+        .then(response => response.json())
+        .then(responseJson => responseJson),
+    );
+
+    // Wait until all jobs are resolved
+    Promise.all(requests).then(responses => setRelatedData(responses));
+  };
+
+  // Navigate to other page
+  const navigateToPage = (name, url) => {
+    navigation.push(`People`, {
+      name,
+      url,
+    });
+  };
+
+  // Computed key based on category which is selected
+  const key = url.includes('films') ? 'title' : 'name';
+
   return (
-    <View style={styles.container}>
-      <Text>FilmsScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {!isLoadingComplete ? (
+        <ActivityIndicator />
+      ) : (
+        <React.Fragment>
+          {detail.map(item => (
+            <React.Fragment key={key}>
+              {/* START detail container */}
+              <View style={styles.detailContainer}>
+                {/* detail container title */}
+                <Text style={styles.bigText}>{item[key]}</Text>
+                <View style={styles.line} />
+
+                {/* START detail attribute container */}
+                <View style={styles.detailAttrContainer}>
+                  {/* START detail attribute top container */}
+                  <ScrollView style={styles.detailAttrTop}>
+                    <Text style={styles.smallText}>{item.opening_crawl}</Text>
+                  </ScrollView>
+                  {/* END detail attribute top container */}
+                  {/* START detail attribute bottom container */}
+                  <View style={styles.detailAttrBottom}>
+                    <View style={styles.detailAttrLabel}>
+                      {/* Attribute labels */}
+                      <Text style={styles.smallText}>Release</Text>
+                      <Text style={styles.smallText}>Episode</Text>
+                      <Text style={styles.smallText}>Director</Text>
+                      <Text style={styles.smallText}>Producer</Text>
+                    </View>
+                    <View style={styles.detailAttrValue}>
+                      {/* Attribute values */}
+                      <Text style={styles.smallText}>: {item.release_date}</Text>
+                      <Text style={styles.smallText}>: {item.episode_id}</Text>
+                      <Text style={styles.smallText}>: {item.director}</Text>
+                      <Text style={styles.smallText}>: {item.producer}</Text>
+                    </View>
+                  </View>
+                  {/* END detail attribute bottom container */}
+                </View>
+                {/* END detail attribute container */}
+              </View>
+              {/* END detail container */}
+
+              {/* START more container */}
+              <View style={styles.moreContainer}>
+                {/* more container title */}
+                <Text style={styles.bigText}>People</Text>
+                <View style={styles.line} />
+
+                {/* more container data */}
+                <ScrollView>
+                  {relatedData.map(item => (
+                    <Text key={item.name} onPress={() => navigateToPage(item.name, item.url)} style={styles.smallText}>
+                      {item.name}
+                    </Text>
+                  ))}
+                </ScrollView>
+              </View>
+              {/* END more container */}
+            </React.Fragment>
+          ))}
+        </React.Fragment>
+      )}
+    </SafeAreaView>
   );
 }
--- screens/Films/styles.js
+++ screens/Films/styles.js
@@ -1,10 +1,87 @@
 import { StyleSheet } from 'react-native';
 
+const primaryColor = '#130f40';
+const secondaryColor = '#fff';
+
 const styles = StyleSheet.create({
+  // Container stylesheets
+
+  // Main containers
   container: {
     flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: primaryColor,
+  },
+  detailContainer: {
+    flex: 2,
+    alignSelf: 'flex-start',
+    alignSelf: 'stretch',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: secondaryColor,
+    margin: 7,
+    padding: 10,
+  },
+  moreContainer: {
+    flex: 1,
+    alignSelf: 'flex-start',
+    alignSelf: 'stretch',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: secondaryColor,
+    margin: 7,
+    padding: 10,
+  },
+
+  // Part of detail container
+  detailAttrContainer: {
+    flex: 1,
+  },
+
+  // Part of detail attribute container
+  detailAttrTop: {
+    flex: 1,
+    flexDirection: 'row',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: secondaryColor,
+    padding: 10,
+    marginBottom: 10,
+  },
+  detailAttrBottom: {
+    flexDirection: 'row',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: secondaryColor,
+    padding: 10,
+  },
+
+  // Part of detail attribute top/bottom container
+  detailAttrLabel: {
+    flex: 1,
+  },
+  detailAttrValue: {
+    flex: 3,
+  },
+
+  // Text stylesheets
+  bigText: {
+    fontSize: 20,
+    color: secondaryColor,
+  },
+  smallText: {
+    fontSize: 15,
+    color: secondaryColor,
+  },
+
+  // Misc stylesheets
+  line: {
+    borderBottomColor: secondaryColor,
+    borderBottomWidth: 1,
+    marginTop: 10,
+    marginBottom: 10,
   },
 });
 
