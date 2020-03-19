40d3ca519014e9769c0b71b382b115ed94e6ff40
Jaya McMoore <hurifajri@gmail.com>
2020-03-19 16:18:07 +0700

    Finish people screen

--- screens/People/PeopleScreen.jsx
+++ screens/People/PeopleScreen.jsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
-import { ActivityIndicator, FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ActivityIndicator, SafeAreaView, ScrollView, Text, View } from 'react-native';
 import { SCHEMA } from '../../constants/Api';
 import styles from './styles';
 
 export default function PeopleScreen({ navigation, route }) {
   const [detail, setDetail] = React.useState([]);
-  const [relatedData, setRelatedData] = React.useState([]);
+  const [relatedSpecies, setRelatedSpecies] = React.useState([]);
+  const [relatedPlanets, setRelatedPlanets] = React.useState([]);
+  const [relatedFilms, setRelatedFilms] = React.useState([]);
   const [isLoadingComplete, setLoadingComplete] = React.useState(false);
   const { name, url } = route.params;
 
@@ -24,6 +26,9 @@ export default function PeopleScreen({ navigation, route }) {
         const responseJson = await response.json();
         const data = [responseJson];
         setDetail(data);
+        fetchRelatedSpecies(data[0].species);
+        fetchRelatedPlanets(data[0].homeworld);
+        fetchRelatedFilms(data[0].films);
       } catch (error) {
         console.error(error);
       } finally {
@@ -32,8 +37,8 @@ export default function PeopleScreen({ navigation, route }) {
     })();
   }, []);
 
-  // Fetch related data of selected detail
-  const fetchRelatedData = urls => {
+  // Fetch related species of selected detail
+  const fetchRelatedSpecies = urls => {
     // map every url to promise of fetch
     let requests = urls.map(url =>
       fetch(url)
@@ -42,7 +47,40 @@ export default function PeopleScreen({ navigation, route }) {
     );
 
     // Wait until all jobs are resolved
-    Promise.all(requests).then(responses => setRelatedData(responses));
+    Promise.all(requests).then(responses => setRelatedSpecies(responses));
+  };
+
+  // Fetch related planets of selected detail
+  const fetchRelatedPlanets = async url => {
+    try {
+      const response = await fetch(`${url}?${SCHEMA}`);
+      const responseJson = await response.json();
+      const data = [responseJson];
+      setRelatedPlanets(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Fetch related films of selected detail
+  const fetchRelatedFilms = urls => {
+    // map every url to promise of fetch
+    let requests = urls.map(url =>
+      fetch(url)
+        .then(response => response.json())
+        .then(responseJson => responseJson),
+    );
+
+    // Wait until all jobs are resolved
+    Promise.all(requests).then(responses => setRelatedFilms(responses));
+  };
+
+  // Navigate to other page
+  const navigateToFilms = (name, url) => {
+    navigation.push(`Films`, {
+      name,
+      url,
+    });
   };
 
   // Computed key based on category which is selected
@@ -53,21 +91,99 @@ export default function PeopleScreen({ navigation, route }) {
       {!isLoadingComplete ? (
         <ActivityIndicator />
       ) : (
-        <View style={styles.detailContainer}>
+        <React.Fragment>
           {detail.map(item => (
             <React.Fragment key={key}>
-              <View style={styles.detail}>
-                <Text style={styles.detailText}>{item[key]}</Text>
+              {/* START detail container */}
+              <View style={styles.detailContainer}>
+                {/* detail container title */}
+                <Text style={styles.bigText}>{item[key]}</Text>
+                <View style={styles.line} />
+
+                {/* START detail attribute container */}
+                <View style={styles.detailAttrContainer}>
+                  <View style={styles.detailAttrLabel}>
+                    {/* Attribute labels */}
+                    <Text style={styles.smallText}>Gender</Text>
+                    <Text style={styles.smallText}>Birth</Text>
+                    <Text style={styles.smallText}>Height</Text>
+                    <Text style={styles.smallText}>Mass</Text>
+                    <Text style={styles.smallText}>Hair color</Text>
+                    <Text style={styles.smallText}>Skin color</Text>
+                    <Text style={styles.smallText}>Eye color</Text>
+                  </View>
+                  <View style={styles.detailAttrValue}>
+                    {/* Attribute values */}
+                    <Text style={styles.smallText}>: {item.gender}</Text>
+                    <Text style={styles.smallText}>: {item.birth_year}</Text>
+                    <Text style={styles.smallText}>: {item.height}</Text>
+                    <Text style={styles.smallText}>: {item.mass}</Text>
+                    <Text style={styles.smallText}>: {item.hair_color}</Text>
+                    <Text style={styles.smallText}>: {item.skin_color}</Text>
+                    <Text style={styles.smallText}>: {item.eye_color}</Text>
+                  </View>
+                </View>
+                {/* END detail attribute container */}
               </View>
-              {fetchRelatedData(item.films)}
-              {relatedData.map(item => (
-                <React.Fragment key={item.title}>
-                  <Text style={styles.detailText}>{item.title}</Text>
-                </React.Fragment>
-              ))}
+              {/* END detail container */}
+
+              {/* START more container species */}
+              <View style={styles.moreContainer}>
+                {/* more container title */}
+                <Text style={styles.bigText}>Species</Text>
+                <View style={styles.line} />
+
+                {/* more container data */}
+                <ScrollView>
+                  {relatedSpecies.map(item => (
+                    <Text key={item.name} style={styles.smallText}>
+                      {item.name}
+                    </Text>
+                  ))}
+                </ScrollView>
+              </View>
+              {/* END more container species */}
+
+              {/* START more container planets */}
+              <View style={styles.moreContainer}>
+                {/* more container title */}
+                <Text style={styles.bigText}>Planets</Text>
+                <View style={styles.line} />
+
+                {/* more container data */}
+                <ScrollView>
+                  {relatedPlanets.map(item => (
+                    <Text key={item.name} style={styles.smallText}>
+                      {item.name}
+                    </Text>
+                  ))}
+                </ScrollView>
+              </View>
+              {/* END more container planets */}
+
+              {/* START more container films */}
+              <View style={styles.moreContainerFlex}>
+                {/* more container title */}
+                <Text style={styles.bigText}>Films</Text>
+                <View style={styles.line} />
+
+                {/* more container data */}
+                <ScrollView>
+                  {relatedFilms.map(item => (
+                    <Text
+                      key={item.title}
+                      onPress={() => navigateToFilms(item.title, item.url)}
+                      style={styles.smallText}
+                    >
+                      {item.title}
+                    </Text>
+                  ))}
+                </ScrollView>
+              </View>
+              {/* END more container films */}
             </React.Fragment>
           ))}
-        </View>
+        </React.Fragment>
       )}
     </SafeAreaView>
   );
--- screens/People/styles.js
+++ screens/People/styles.js
@@ -1,28 +1,78 @@
 import { StyleSheet } from 'react-native';
 
+const primaryColor = '#130f40';
+const secondaryColor = '#fff';
+
 const styles = StyleSheet.create({
+  // Container stylesheets
+
+  // Main containers
   container: {
     flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#130f40',
+    backgroundColor: primaryColor,
   },
   detailContainer: {
-    flex: 1,
-    margin: 10,
-  },
-  detail: {
-    flex: 1,
+    alignSelf: 'flex-start',
+    alignSelf: 'stretch',
     borderStyle: 'solid',
     borderWidth: 1,
-    borderColor: '#fff',
-    width: '100%',
+    borderColor: secondaryColor,
+    margin: 7,
+    padding: 10,
   },
-  detailText: {
+  moreContainer: {
+    alignSelf: 'flex-start',
+    alignSelf: 'stretch',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: secondaryColor,
+    margin: 7,
+    padding: 10,
+  },
+  moreContainerFlex: {
+    flex: 1,
+    alignSelf: 'flex-start',
+    alignSelf: 'stretch',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: secondaryColor,
+    margin: 7,
+    padding: 10,
+  },
+
+  // Part of detail container
+  detailAttrContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+
+  // Part of detail attribute container
+  detailAttrLabel: {
+    flex: 1,
+  },
+  detailAttrValue: {
+    flex: 2,
+  },
+
+  // Text stylesheets
+  bigText: {
     fontSize: 20,
-    color: '#fff',
-    textAlign: 'left',
-    fontWeight: 'bold',
+    color: secondaryColor,
+  },
+  smallText: {
+    fontSize: 15,
+    color: secondaryColor,
+  },
+
+  // Misc stylesheets
+  line: {
+    borderBottomColor: secondaryColor,
+    borderBottomWidth: 1,
+    marginTop: 10,
+    marginBottom: 10,
   },
 });
 
