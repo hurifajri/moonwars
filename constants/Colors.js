7ec5505472c25c54dbd7fbc770580976bcec41e3
Jaya McMoore <hurifajri@gmail.com>
2020-03-21 08:44:11 +0700

    Change color variable

--- constants/Colors.js
+++ constants/Colors.js
@@ -2,9 +2,9 @@ const tintColor = '#2f95dc';
 
 export default {
   tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
+  drawerIconDefault: '#ccc',
+  drawerIconSelected: tintColor,
+  drawer: '#fefefe',
   errorBackground: 'red',
   errorText: '#fff',
   warningBackground: '#EAEB5E',
